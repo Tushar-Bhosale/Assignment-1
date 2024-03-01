e0a530a2e6fb6f5065c8e1f59c33d433b8f3d51b
Tushar-Bhosale <84708098+Tushar-Bhosale@users.noreply.github.com>
2024-03-01 09:46:08 +0530

    Add files via upload

---
+++ Day 1 lab/Addition.java
@@ -0,0 +1,13 @@
+class Addition{
+	public static void main(String arg[]){
+
+		int number1 = 10;
+		int number2 = 20;
+		int add;
+		add = number1 + number2;
+		System.out.println("Addition of Numbers");
+		System.out.println("add");
+	
+	} //main method end
+
+} //class end
